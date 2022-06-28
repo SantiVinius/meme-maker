25ab106fe108f9a276cf7c4da4c84bd386a8af83
SantiVinius <backup.vinius@gmail.com>
2022-06-28 10:09:02 -0300

    projeto finalizado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+
+import GlobalStyles from './styles/global'
+import Home from './pages/Home/index'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <GlobalStyles />
+            <Home /> 
+        </>
+    )
 }
 
-export default App;
+export default App
